4da8ff29487999cb49d1af8573729f268a8d6d00
Jaskaran1519 <jaskaran1519@gmail.com>
2025-02-06 15:34:33 +0530

    ojsadfbvn

--- app/(front)/cart/CartDetails.tsx
+++ app/(front)/cart/CartDetails.tsx
@@ -3,7 +3,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { Suspense, useEffect, useState } from 'react';
 
 import useCartService from '@/lib/hooks/useCartStore';
 
@@ -19,90 +19,92 @@ const CartDetails = () => {
   if (!mounted) return <>Loading...</>;
 
   return (
-    <div>
-      <h1 className='py-4 text-2xl'>Shopping Cart</h1>
-      {items.length === 0 ? (
-        <div>
-          <p className='mb-2'>Cart is empty :(</p>
-          <Link href='/' className='btn'>
-            Go shopping
-          </Link>
-        </div>
-      ) : (
-        <div className='grid md:grid-cols-4 md:gap-5'>
-          <div className='overflow-x-auto md:col-span-3'>
-            <table className='table'>
-              <thead>
-                <tr>
-                  <th>Item</th>
-                  <th>Quantity</th>
-                  <th>Price</th>
-                </tr>
-              </thead>
-              <tbody>
-                {items.map((item) => (
-                  <tr key={item.slug}>
-                    <td className='flex items-center'>
-                      <Link
-                        href={`/product/${item.slug}`}
-                        className='flex items-center'
-                      >
-                        <Image
-                          src={item.image}
-                          alt={item.name}
-                          width={50}
-                          height={50}
-                        />
-                      </Link>
-                      <span className='px-2'>{item.name}</span>
-                    </td>
-                    <td>
-                      <div>
-                        <button
-                          className='btn'
-                          type='button'
-                          onClick={() => decrease(item)}
-                        >
-                          -
-                        </button>
-                        <span className='px-2'>{item.qty}</span>
-                        <button
-                          className='btn'
-                          type='button'
-                          onClick={() => increase(item)}
-                        >
-                          +
-                        </button>
-                      </div>
-                    </td>
-                    <td>$ {item.price}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
+    <Suspense fallback={<div>Loading cart...</div>}>
+      <div>
+        <h1 className='py-4 text-2xl'>Shopping Cart</h1>
+        {items.length === 0 ? (
+          <div>
+            <p className='mb-2'>Cart is empty :(</p>
+            <Link href='/' className='btn'>
+              Go shopping
+            </Link>
           </div>
-          <div className='card' bg-base-300>
-            <div className='card-body'>
-              <ul>
-                <li className='pb-3 text-xl'>
-                  Subtotal: {items.reduce((acc, item) => acc + item.qty, 0)}:
-                  <br />$ {itemsPrice}
-                </li>
-                <li>
-                  <button
-                    type='button'
-                    className='btn btn-primary w-full'
-                    onClick={() => router.push('/shipping')}
-                  >
-                    Proceed to Checkout
-                  </button>
-                </li>
-              </ul>
+        ) : (
+          <div className='grid md:grid-cols-4 md:gap-5'>
+            <div className='overflow-x-auto md:col-span-3'>
+              <table className='table'>
+                <thead>
+                  <tr>
+                    <th>Item</th>
+                    <th>Quantity</th>
+                    <th>Price</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {items.map((item) => (
+                    <tr key={item.slug}>
+                      <td className='flex items-center'>
+                        <Link
+                          href={`/product/${item.slug}`}
+                          className='flex items-center'
+                        >
+                          <Image
+                            src={item.image}
+                            alt={item.name}
+                            width={50}
+                            height={50}
+                          />
+                        </Link>
+                        <span className='px-2'>{item.name}</span>
+                      </td>
+                      <td>
+                        <div>
+                          <button
+                            className='btn'
+                            type='button'
+                            onClick={() => decrease(item)}
+                          >
+                            -
+                          </button>
+                          <span className='px-2'>{item.qty}</span>
+                          <button
+                            className='btn'
+                            type='button'
+                            onClick={() => increase(item)}
+                          >
+                            +
+                          </button>
+                        </div>
+                      </td>
+                      <td>$ {item.price}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+            <div className='card' bg-base-300>
+              <div className='card-body'>
+                <ul>
+                  <li className='pb-3 text-xl'>
+                    Subtotal: {items.reduce((acc, item) => acc + item.qty, 0)}:
+                    <br />$ {itemsPrice}
+                  </li>
+                  <li>
+                    <button
+                      type='button'
+                      className='btn btn-primary w-full'
+                      onClick={() => router.push('/shipping')}
+                    >
+                      Proceed to Checkout
+                    </button>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
-        </div>
-      )}
-    </div>
+        )}
+      </div>
+    </Suspense>
   );
 };
 
--- app/(front)/cart/page.tsx
+++ app/(front)/cart/page.tsx
@@ -1,20 +1,19 @@
 import { Metadata } from 'next';
 import { Suspense } from 'react';
 import dynamic from 'next/dynamic';
+import CartDetails from './CartDetails';
 
 export const metadata: Metadata = {
   title: 'Shopping Cart',
 };
-const CartDetails = dynamic(() => import('./CartDetails'), {
-  loading: () => <div>Loading your orders...</div>,
-  ssr: false, // Ensures this component is only rendered on the client side
-});
+// const CartDetails = dynamic(() => import('./CartDetails'), {
+//   loading: () => <div>Loading your orders...</div>,
+//   ssr: false, // Ensures this component is only rendered on the client side
+// });
 const CartPage = () => {
   return (
     <div>
-      <Suspense fallback={<div>Loading cart details...</div>}>
         <CartDetails />
-      </Suspense>
     </div>
   );
 };
