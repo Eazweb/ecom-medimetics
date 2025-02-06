31bec9ad57d292cbb57da1bc7b6a38e1e4cbb1d6
Jaskaran1519 <jaskaran1519@gmail.com>
2025-02-06 15:47:31 +0530

    oskdnfsod

--- app/(front)/cart/page.tsx
+++ app/(front)/cart/page.tsx
@@ -1,19 +1,22 @@
 import { Metadata } from 'next';
 import { Suspense } from 'react';
 import dynamic from 'next/dynamic';
-import CartDetails from './CartDetails';
 
 export const metadata: Metadata = {
   title: 'Shopping Cart',
 };
-// const CartDetails = dynamic(() => import('./CartDetails'), {
-//   loading: () => <div>Loading your orders...</div>,
-//   ssr: false, // Ensures this component is only rendered on the client side
-// });
+
+const CartDetails = dynamic(() => import('./CartDetails'), {
+  loading: () => <div>Loading your orders...</div>,
+  ssr: false,
+});
+
 const CartPage = () => {
   return (
     <div>
+      <Suspense fallback={<div>Loading your orders...</div>}>
         <CartDetails />
+      </Suspense>
     </div>
   );
 };
